1a276fb6e8850fa9ec9ea68cf23fc2c8f0a46e28
28aigerim <169145722+28aigerim@users.noreply.github.com>
2025-04-23 23:28:34 +0600

    Delete Aigerim/UNI/Assignments 24-25/OOP/assignment5 directory

--- Aigerim/UNI/Assignments 24-25/OOP/assignment5/tests/test_cart.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from cart import Cart
-
-
-class TestCart(unittest.TestCase):
-
-    def test_get_total_price(self):
-        self.assertEqual(Cart.get_total_price(), 'The total price in the cart is 0 soms.', 'Something wrong.')
-
